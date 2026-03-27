87af7a202438aba9dce11064ef1def12f4e9c26b
Levi 태선 Brown <levitkbrown@gmail.com>
2026-03-26 19:30:08 -0700

    actually pipe it

---
+++ dbgdrummer.py
@@ -0,0 +1,19 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the audio file
+y, sr = librosa.load(librosa.ex('choice'))
+
+# Calculate RMS energy
+rms = librosa.feature.rms(y=y)
+
+# Plotting the RMS energy
+plt.figure(figsize=(10, 4))
+plt.plot(rms[0], label='RMS Energy')
+plt.title('Audio Energy Over Time (RMS)')
+plt.ylabel('Energy')
+plt.xlabel('Frame')
+plt.legend()
+plt.show()
